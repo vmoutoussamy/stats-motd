de784b42a0b2fbea0705099cce6c78f4fb2450d4
gameboy974 <vmoutoussamy@suse.com>
2016-01-04 17:12:24 +0100

    Polishing Memory and swap Usage by using a new method

--- stats-motd.sh
+++ stats-motd.sh
@@ -6,10 +6,15 @@ echo -e "
 \033[0;35m+    \033[0;38mKernel \033[0;35m= \033[1;32m`uname -r`
 \033[0;35m+    \033[0;38mUptime \033[0;35m= \033[1;32m`uptime | sed 's/.*up ([^,]*), .*/1/'`
 \033[0;35m+++++++++++++++++: \033[0;38mMemory\033[0;35m :+++++++++++++++++++
-\033[0;35m+    \033[0;38mMemory \033[0;35m= \033[1;32m`free -m | grep Mem: | awk '{print "Total:",$2}' && free -m | grep '\-\/+' | awk '{print "Free:",$4}'`
-\033[0;35m+    \033[0;38mSwap \033[0;35m= \033[1;32m`free -m | grep Swap: | awk '{print "Total:", $2, "Used:", $3}'`
+\033[1;32m`free -k | {
+  read ; read TITLE TOTAL USED FREE SHARED BUFFERS CACHED
+  echo "Memory Usage: $(( 100 * ($TOTAL - $FREE - $BUFFERS - $CACHED) / $TOTAL ))%"
+
+  read ; read TITLE TOTAL USED REST
+  echo "Swap Usage: $(( 100 * $USED / $TOTAL ))%"
+}`
 \033[0;35m++++++++++++++++++: \033[0;38mDisks\033[0;35m :++++++++++++++++++++
-+  \033[1;32m`df -h | egrep -v 'tmpfs|.snapshots' | awk '{print $1,$2,$5,$6}' | column -t`
++  \033[1;32m`df -h | egrep -v 'tmpfs|.snapshots' | awk '{print $1,$5,$6}' | column -t`
 \033[0;35m++++++++++++++++++: \033[0;38mZombies\033[0;35m :++++++++++++++++++++
 \033[0;32m`ps -el | grep Z | grep -v WCHAN`
 "
